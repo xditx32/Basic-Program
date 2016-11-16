bf22cd97af64891b358bf530cba728bc99edd8ae
xditx32 <xditx32@gmail.com>
2016-11-16 13:48:35 +0700

    :book: Prak_1 Hello

---
+++ Prak_1/Hello.cpp
@@ -0,0 +1,16 @@
+/* Praktek membuat Program Cetak : Hello World
+ * Copyright (C) 2014 Aditya Nur Rohman 
+ * Discovered by <aditya.nurrohman@gmail.com>
+ * Platform : Ubuntu
+ * Compile with :
+ * g++ Hello.cpp -o Hello
+*/
+#include <iostream>
+
+using namespace std;
+main(){        
+	/*Fungsi untuk menampilkan tulisan*/
+        cout << "Hello World \n";
+
+return 0;
+}
